a8bbc10d5f33d6b96ffe1ce3a71fcd74266e8c9c
Komecitos <irga.prayoga273@gmail.com>
2026-08-31 11:11:36 +0700

    chore: perbarui konfigurasi build dan deeplink NativePHP

--- config/nativephp.php
+++ config/nativephp.php
@@ -13,7 +13,7 @@ return [
      * usually in the form of a reverse domain name.
      * For example: com.nativephp.app
      */
-    'app_id' => env('NATIVEPHP_APP_ID', 'com.nativephp.app'),
+    'app_id' => env('NATIVEPHP_APP_ID', 'com.asramaapp.app'),
 
     /**
      * If your application allows deep linking, you can specify the scheme
@@ -24,12 +24,12 @@ return [
      * This would allow you to open your application using a URL like:
      * nativephp://some/path
      */
-    'deeplink_scheme' => env('NATIVEPHP_DEEPLINK_SCHEME'),
+    'deeplink_scheme' => env('NATIVEPHP_DEEPLINK_SCHEME', 'asramaapp'),
 
     /**
      * The author of your application.
      */
-    'author' => env('NATIVEPHP_APP_AUTHOR'),
+    'author' => env('NATIVEPHP_APP_AUTHOR', 'Irga Prayoga'),
 
     /**
      * The copyright notice for your application.
